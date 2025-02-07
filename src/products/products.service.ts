a54382dbe5c00e6b393ed5bfea43960f3f2e2356
chenwl <13751840799@163.com>
2025-02-07 15:37:27 +0800

    feat: Serve Static

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,23 +1,41 @@
+import { promises as fs } from 'fs';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductRequest } from './dto/create-product.request';
+import { join } from 'path';
 
 @Injectable()
 export class ProductsService {
-  
   constructor(private readonly prismaService: PrismaService) {}
-  
-  createProduct(data: CreateProductRequest, userId: number){
+
+  createProduct(data: CreateProductRequest, userId: number) {
     return this.prismaService.product.create({
       data: {
         ...data,
-        userId
-      }
-    })
+        userId,
+      },
+    });
   }
-    
-  async getProducts(){
-    return this.prismaService.product.findMany()
+
+  async getProducts() {
+    const products = await this.prismaService.product.findMany();
+    return Promise.all(
+      products.map(async (product) => ({
+        ...product,
+        imageExists: await this.imageExists(product.id),
+      })),
+    );
   }
-  
+
+  private async imageExists(productId: number) {
+    try {
+      await fs.access(
+        join(__dirname, '../../', `public/products/${productId}.jpg`),
+        fs.constants.F_OK,
+      );
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
 }
